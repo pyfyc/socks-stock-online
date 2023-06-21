335b04939149434467eda126d62a9268dd3aaef3
Alex <alexeym75@gmail.com>
2023-06-21 15:51:18 +0700

    Initial commit

---
+++ src/main/java/pro/sky/socksstockonline/SocksStockOnlineApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.socksstockonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStockOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStockOnlineApplication.class, args);
+    }
+
+}
